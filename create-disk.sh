f62835521cd852dc747c5d0877f5960050d12b69
IlyaShurupov <ilusha@workspace.com>
2024-05-23 14:59:06 +0300

    stable

--- create-disk.sh
+++ create-disk.sh
@@ -1,47 +1,36 @@
+#!/bin/bash
+
+# Create disk image
+dd if=/dev/zero of=disk.img bs=1M count=600 status=progress
+
+# Partition disk image
+parted -s disk.img mklabel gpt
+parted -s disk.img mkpart boot fat32 0% 100%
+parted -s disk.img set 1 boot on
+
+# Associate loop device
 DEV=$(losetup -f)
+sudo losetup $DEV disk.img
 
-rm disk.img
+# Create partition device maps
+sudo kpartx -av $DEV
 
-#qemu-img create disk.img 600M
-dd if=/dev/zero of=disk.img status=progress count=600MB
+# Wait for partitions to be available
+sleep 2
 
-parted disk.img << EFO
-mklabel
-gpt
-mkpart
-boot
-fat32
-0
-100%
-set 1 boot on
-EFO
+PART=/dev/mapper/$(basename $DEV)p1
 
-sudo losetup --detach-all
-sudo losetup $DEV -o 17408 disk.img
-sudo mkfs.fat -F 32 $DEV
+sudo mkfs.fat -F 32 $PART 
 
-sudo mkdir -p mnt
-sudo mount $DEV ./mnt
-#sudo mkdir -p mnt/EFI/BOOT
-sudo grub-install --boot-directory=mnt --efi-directory=mnt --bootloader-id=BOOT --target=x86_64-efi
+# Mount the first partition
+sudo mount  $PART ./mnt
+
+sudo grub-install --boot-directory=mnt --efi-directory=mnt --bootloader-id=BOOT --target=x86_64-efi --removable
 
 sudo cp grub.cfg mnt/grub/grub.cfg
 sudo cp kernel.bin mnt/kernel.bin
 
-
-#sudo rm -rf mnt/*
-#sudo cp -r /boot/* mnt/
-#sudo cp -r /boot/grub/grub.cfg mnt/grub/grub.cfg
-
-#sudo rm -rf mnt/EFI
-#sudo cp -r /boot/EFI mnt/EFI
-#sudo rm -rf mnt/EFI/--removable
-#sudo ls mnt/EFI
-
-#sudo mv mnt/EFI/BOOT/grubx64.efi mnt/EFI/BOOT/BOOTX64.EFI
-
-sudo cp BOOTX64.EFI mnt/EFI/BOOT/BOOTX64.EFI
-
-
-sudo umount mnt
-sudo losetup -d $DEV
+# Unmount partition and clean up
+sudo umount ./mnt
+sudo kpartx -d disk.img
+#sudo losetup -d $DEV
