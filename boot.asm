c906ea7bb0f2fd82c75c40c2e545c0ef7ad4cf76
IlyaShurupov <ilusha@workspace.com>
2024-05-24 22:13:28 +0300

    testing long mode

--- boot.asm
+++ boot.asm
@@ -26,9 +26,62 @@ section .text
     push ebx
     push eax
 
-		call kernel_main
+		; call kernel_main
+   call check_cpuid
+   call check_extended_cpuid
+   call check_long_mode
 
-		cli
+	cli
 	loop:	hlt
 		jmp loop
 
+check_extended_cpuid:
+  mov eax, 0x80000000
+  CPUID
+  cmp eax, 0x80000001
+  jl  NoLongMode
+  ret
+
+check_long_mode:
+  mov eax,  0x80000001
+  CPUID
+  and edx, 1 << 29
+  cmp edx, 0
+  jng NoLongMode
+  ret
+
+check_cpuid:
+    ; Check if CPUID is supported by attempting to flip the ID bit (bit 21) in
+    ; the FLAGS register. If we can flip it, CPUID is available.
+
+    ; Copy FLAGS into EAX via stack
+    pushfd
+    pop eax
+
+    ; Copy to ECX as well for comparing later on
+    mov ecx, eax
+
+    ; Flip the ID bit
+    xor eax, 1 << 21
+
+    ; Copy EAX to FLAGS via the stack
+    push eax
+    popfd
+
+    ; Copy FLAGS back to EAX (with the flipped bit if CPUID is supported)
+    pushfd
+    pop eax
+
+    ; Restore FLAGS from the old version stored in ECX (i.e. flipping the ID bit
+    ; back if it was ever flipped).
+    push ecx
+    popfd
+
+    ; Compare EAX and ECX. If they are equal then that means the bit wasn't
+    ; flipped, and CPUID isn't supported.
+    cmp eax, ecx
+    je NoLongMode
+    ret
+
+NoLongMode:
+  jmp loop
--- kernel.c
+++ kernel.c
@@ -11,16 +11,16 @@
 #endif
 
 
-void kernel_main(unsigned long magic, unsigned long addr)
+void kernel_main(unsigned long magic, unsigned long)
 {
-  multiboot_info_t *mbi;
+  // multiboot_info_t *mbi;
   
   if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
     return;
   }
 
   /* Set MBI to the address of the Multiboot information structure. */
-  mbi = (multiboot_info_t *) addr;
+   // mbi = (multiboot_info_t *) addr;
 
 
 
